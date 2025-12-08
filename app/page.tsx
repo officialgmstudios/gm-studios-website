67d7d28b90a261f44c8fdb671bd57a3312efa0d8
coder-tejas <tejashvagupta1@gmail.com>
2025-12-08 20:40:45 +0530

    feat: add glassmorphism navbar for hero

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,17 @@
 // app/page.js
+import Navbar from "@/components/Navbar";
 import Hero from "../components/LandingPage/Hero";
 import Manifesto from "../components/Manifesto/Manisfesto";
-import Network from "../components/LandingPage/Networks";
-import Projects from "../components/LandingPage/Projects";
+// import Network from "../components/LandingPage/Networks";
+// import Projects from "../components/LandingPage/Projects";
 export default function HomePage() {
   return (
     <main>
+      <Navbar />
       <Hero />
       <Manifesto />
-      <Network />
-      <Projects />
+      {/* <Network /> */}
+      {/* <Projects /> */}
     </main>
   );
 }
